e60bccdb66ac1a5a37402a72b16ffb6bedcf684d
Sean Timothy Noonan <stnoonan@obsolescence.net>
2011-12-21 19:34:35 -0500

    solved

---
+++ 5.rb
@@ -0,0 +1,7 @@
+#520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# Needs all primes
+# Needs 16 and 9 (highest power of 2 and 3 below 20)
+n = 1*16*9*5*7*11*13*17*19
+puts n
